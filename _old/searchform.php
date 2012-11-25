bdcc98b9d11c5f82e56991ac23c75fcad8d976f3
Norcross <andrew@andrewnorcross.com>
2012-11-24 23:37:11 -0500

    removing a folder

--- _old/searchform.php
+++
@@ -1,5 +0,0 @@
-<form action="<?php bloginfo('url'); ?>/" id="searchform" method="get" role="search" class="navbar-search pull-left">
-		<input class="search-query span2" type="text" id="s" name="s" value="" placeholder="Search">
-		
-	<!-- <input type="submit" value="Search" id="searchsubmit"> -->
-</form>
